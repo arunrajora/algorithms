b94ccca1fb609bc3bd6e67b55ae1986edb034b06
arunrajora <rajora.rajora2@outlook.com>
2017-01-31 07:14:20 +0530

    create generator for a single code file for reference.

--- generator.cpp
+++ generator.cpp
@@ -1,13 +1,80 @@
 #include<iostream>
 #include<fstream>
 #include<ctime>
-#include<cstring>
+#include<string>
 using namespace std;
 
 const string OUT_DIR = "outputs";
+const string INP_DIR = "codes";
+const string MAPPER_FILE_NAME = "mapper.txt";
+
+ofstream outFile;
+long fileNumber=0,dirNumber=0,errorNumber=0;
 
 string get_file_name(){
 	return "CODES_"+to_string(time(NULL))+".txt";
+}
+
+void dumpFileContents(string dirName, string fileName){
+	if(fileName.length()<0){
+		return ;
+	}
+	
+	string filePath;
+	
+	if(dirName.length()>0){
+		filePath=dirName + "\\" + fileName;
+	}
+	else{
+		filePath= ".\\" + fileName;
+	}
+	cout<<"\tDUMPING FILE("<<(++fileNumber)<<"): "<<filePath<<endl;
+	ifstream codeFile;
+	codeFile.open(filePath);
+	if(codeFile != NULL){
+		outFile<<"// __________________________"<<endl<<endl;
+		outFile<<"// "<<filePath<<endl;
+		
+		string contents;
+		
+		while(!codeFile.eof()){
+			getline(codeFile,contents);
+			outFile<<contents<<endl;
+		}
+		
+		outFile<<"// __________________________"<<endl<<endl;
+	}
+	else{
+		errorNumber++;
+		cout<<"ERROR: opening file : "<<filePath<<endl;
+	}
+}
+
+void recurseProcessDirectory(string dirName){
+	if(dirName.length()>0){
+		ifstream dirMapper;
+		dirMapper.open(dirName + "\\" + MAPPER_FILE_NAME);
+		if(dirMapper==NULL){
+			errorNumber++;
+			cout<<"ERROR: failed to find mapper file at : "+dirName<<endl;
+		}
+		else{
+			cout<<"Processing directory("<<(++dirNumber)<<"): "<<dirName<<endl;
+			string entityName;
+			while(!dirMapper.eof()){
+			   dirMapper>>entityName;
+			   if(entityName.length()<=0) continue;
+			   
+			   if(entityName.find(".")>=0 && entityName.find(".")<entityName.length()){
+				dumpFileContents(dirName,entityName);
+			   }
+			   else{
+			   	recurseProcessDirectory(dirName + "\\" + entityName);
+			   }
+			}
+			dirMapper.close();
+		}
+	}
 }
 
 int main(){
@@ -16,19 +83,17 @@ int main(){
 	cout<<"output at file : "<<filename<<endl;
 	
 	string outputPath=".\\" + OUT_DIR + "\\" + filename;
-	ofstream outFile;
 	outFile.open(outputPath);
 	
 	if(outFile != NULL){
-		
-		//process files of codes
-		
+		recurseProcessDirectory(".\\" + INP_DIR);
 		outFile.close();
-		cout<<"Writing Completed !";
+		cout<<endl<<"Writing Completed !"<<endl;
 	}
 	else{
 		cout<<"Error opening file : "<<outputPath<<endl;
-		cout<<"Progream failed!";
+		cout<<endl<<"Program failed!"<<endl;
 	}
+	cout<<errorNumber<<" errors ."<<endl<<dirNumber<<" directorie(s) and "<<fileNumber<<" file(s) processed."<<endl;
 	return 0;
 }
