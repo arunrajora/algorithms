42eab0cc44378c90c12564d9dbaef7ee8e59b100
arunrajora <rajora.rajora2@outlook.com>
2017-02-15 12:46:48 +0530

    add depth first search on graph code.

---
+++ codes/graphs/dfs.cpp
@@ -0,0 +1,45 @@
+
+// Depth First Search
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+enum status{unvisited,visited};
+
+#define N 100005
+vector<int> graph[N];
+status vis[N];
+
+void rdfs(int u){
+	cout<<" at "<<u<<endl;
+	
+	vis[u]=visited;
+	for(int j=0;j<graph[u].size();j++){
+		int v=graph[u][j];
+		if(!vis[v]){
+			rdfs(v);
+		}
+	}
+}
+
+void dfs(int G_size){
+	for(int i=0;i<G_size;i++){
+		if(!vis[i]){
+			rdfs(i);
+		}
+	}
+}
+
+int main(){
+	graph[1].push_back(2);
+	graph[1].push_back(4);
+	graph[2].push_back(1);
+	graph[2].push_back(3);
+	graph[3].push_back(2);
+	graph[4].push_back(1);
+	graph[5].push_back(6);
+	graph[6].push_back(5);
+	
+	dfs(7);
+}
