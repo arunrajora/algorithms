49a5c3fa598ed26e8848660938fb462e8f7034e4
arunrajora <rajora.rajora2@outlook.com>
2017-02-22 15:44:54 +0530

    update output file.

--- outputs/CODES_1487758458.cpp
+++ outputs/CODES_1487758458.cpp
@@ -1385,6 +1385,206 @@ int main(){
 
 // __________________________
 
+// .\codes\graphs\maximum_flow\edmonds_karp.cpp
+
+// Edmonds Karp
+// maximum flow in a graph
+// 0(V^3E) implementation
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define N 1005
+#define INF 100000000
+
+int graph[N][N];
+int res[N][N];
+vector<int> parent;
+cleargraph(int r,int c,int val){for(int i=0;i<r;i++) for(int j=0;j<c;j++) graph[i][j]=val;}
+
+void augment( int v,int s,int minEdge,int& f){
+	if(v==s){
+		f=minEdge;
+	}
+	else if(parent[v]!=-1){
+		augment(parent[v],s,min(minEdge,res[parent[v]][v]),f);
+		res[parent[v]][v]-=f;
+		res[v][parent[v]]+=f;
+	}
+}
+
+int max_flow(int s,int t,int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			res[i][j]=graph[i][j];
+		}
+	}
+	int mf=0;
+	while(true){
+		int f=0;
+		vector<int> dist(n,INF);
+		dist[s]=0;
+		queue<int> q;
+		q.push(s);
+		parent.assign(n,-1);
+		while(!q.empty()){
+			int u=q.front();
+			q.pop();
+			if(u==t) break;
+			for(int v=0;v<n;v++){
+				if(res[u][v]>0 && dist[v]==INF){
+					dist[v]=dist[u]+1;
+					q.push(v);
+					parent[v]=u;
+				}
+			}
+		}
+		augment(t,s,INF,f);
+		if(f==0) break;
+		mf+=f;
+	}
+	return mf;
+}
+
+int main(){
+	
+	cleargraph(7,7,0);
+	graph[0][1]=100;
+	graph[0][5]=1;
+	graph[1][2]=20;
+	graph[1][4]=10;
+	graph[2][4]=20;
+	graph[2][3]=10;
+	graph[5][6]=10;
+	graph[6][4]=1;
+
+	cout<<max_flow(0,4,7)<<endl;
+}
+
+// __________________________
+
+
+
+// __________________________
+
+// .\codes\graphs\maximum_flow\minimum_cut.cpp
+
+// minimum cut in a flow
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define N 1005
+#define INF 100000000
+
+int graph[N][N];
+int res[N][N];
+vector<int> parent;
+vector<bool> vis;
+cleargraph(int r,int c,int val){for(int i=0;i<r;i++) for(int j=0;j<c;j++) graph[i][j]=val;}
+
+void augment( int v,int s,int minEdge,int& f){
+	if(v==s){
+		f=minEdge;
+	}
+	else if(parent[v]!=-1){
+		augment(parent[v],s,min(minEdge,res[parent[v]][v]),f);
+		res[parent[v]][v]-=f;
+		res[v][parent[v]]+=f;
+	}
+}
+
+int max_flow(int s,int t,int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			res[i][j]=graph[i][j];
+		}
+	}
+	int mf=0;
+	while(true){
+		int f=0;
+		vector<int> dist(n,INF);
+		dist[s]=0;
+		queue<int> q;
+		q.push(s);
+		parent.assign(n,-1);
+		while(!q.empty()){
+			int u=q.front();
+			q.pop();
+			if(u==t) break;
+			for(int v=0;v<n;v++){
+				if(res[u][v]>0 && dist[v]==INF){
+					dist[v]=dist[u]+1;
+					q.push(v);
+					parent[v]=u;
+				}
+			}
+		}
+		augment(t,s,INF,f);
+		if(f==0) break;
+		mf+=f;
+	}
+	return mf;
+}
+
+void rdfs(int s,int n){
+	vis.assign(n,false);
+	vis[s]=true;
+	queue<int> q;
+	q.push(s);
+	while(!q.empty()){
+		int u=q.front();
+		q.pop();
+		for(int v=0;v<n;v++){
+			if(!vis[v] && res[u][v]>0){
+				q.push(v);
+				vis[v]=true;
+			}
+		}
+	}
+}
+
+int min_cut(int s,int t,int n){
+	int mcut=0;
+	max_flow(s,t,n);
+	rdfs(s,n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(vis[i] && !vis[j] && graph[i][j]>0){
+				mcut+=graph[i][j];
+			}
+		}
+	}
+	return mcut;
+}
+
+int main(){
+	
+	cleargraph(7,7,0);
+	graph[0][1]=100;
+	graph[0][5]=1;
+	graph[1][2]=20;
+	graph[1][4]=10;
+	graph[2][4]=20;
+	graph[2][3]=10;
+	graph[5][6]=10;
+	graph[6][4]=1;
+
+	cout<<min_cut(0,4,7)<<endl;
+}
+
+// __________________________
+
+
+
+// __________________________
+
 // .\codes\graphs\misc\articulation_point_bridges.cpp
 
 // Articulation point and bridges
@@ -2116,6 +2316,54 @@ int main(){
 	graph[4].push_back(make_pair(10,1));
 	graph[5].push_back(make_pair(10,6));
 	graph[6].push_back(make_pair(10,5));
+	sssp(0,7);
+	for(int i=0;i<7;i++){
+		cout<<i<<" "<<dist[i]<<endl;
+	}
+}
+
+// __________________________
+
+
+
+// __________________________
+
+// .\codes\graphs\single_source_shortest_path\sssp_dag.cpp
+
+// single sorurce shortest path(sssp) on a dag
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define N 100005
+#define inf 100000000
+//item->weight,node
+vector<pair<int,int> > graph[N];
+vector<int> dist;
+
+void rdfs(int s){
+	for(int i=0;i<graph[s].size();i++){
+		pair<int,int> u=graph[s][i];
+		dist[u.second]=min(dist[u.second],dist[s]+u.first);
+		rdfs(u.second);
+	}
+}
+
+void sssp(int s,int n){
+	dist.assign(n,inf);
+	dist[s]=0;
+	rdfs(s);
+}
+
+int main(){
+	graph[0].push_back(make_pair(10,1));
+	graph[1].push_back(make_pair(10,2));
+	graph[1].push_back(make_pair(10,4));
+	graph[3].push_back(make_pair(10,2));
+	graph[5].push_back(make_pair(10,6));
 	sssp(0,7);
 	for(int i=0;i<7;i++){
 		cout<<i<<" "<<dist[i]<<endl;
