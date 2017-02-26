0cf8c12d58b8f5548760d1567f5ddbbd0c95d6d0
arunrajora <rajora.rajora2@outlook.com>
2017-02-27 04:56:35 +0530

    add dinic max flow code.

--- codes/graphs/maximum_flow/dinic.cpp
+++ codes/graphs/maximum_flow/dinic.cpp
@@ -1,77 +1,92 @@
 
-// Edmonds Karp
+// dinic
 // maximum flow in a graph
-// 0(V^3E) implementation
+// 0(V^2E) implementation
 
 #include<iostream>
 #include<vector>
-#include<queue>
 #include<algorithm>
+#include<queue>
 using namespace std;
 
 #define N 1005
-#define INF 100000000
+#define INF 0x3f3f3f3f
 
-int graph[N][N];
-int res[N][N];
-vector<int> parent;
-cleargraph(int r,int c,int val){for(int i=0;i<r;i++) for(int j=0;j<c;j++) graph[i][j]=val;}
+struct edge{
+	int source;
+	int destination;
+	int rev_pos;
+	int capacity;
+	int flow;
+};
+vector<edge> graph[N];
+vector<int> ptr;
+vector<int> dist;
 
-void augment( int v,int s,int minEdge,int& f){
-	if(v==s){
-		f=minEdge;
+void add_edge(int s,int d,int c){
+	graph[s].push_back((edge){s,d,graph[d].size(),c,0});
+	graph[d].push_back((edge){d,s,graph[s].size()-1,0,0});
+}
+
+int dbfs(int s,int t,int n){
+	dist.assign(n,-1);
+	dist[s]=0;
+	queue<int> q;
+	q.push(s);
+	while(!q.empty()){
+		int u=q.front();
+		q.pop();
+		for(int i=0;i<graph[u].size();i++){
+			edge& e=graph[u][i];
+			if(dist[e.destination]<0 && e.flow<e.capacity){
+				dist[e.destination]=dist[u]+1;
+				q.push(e.destination);
+			}
+		}
 	}
-	else if(parent[v]!=-1){
-		augment(parent[v],s,min(minEdge,res[parent[v]][v]),f);
-		res[parent[v]][v]-=f;
-		res[v][parent[v]]+=f;
+	return dist[t]>=0;
+}
+
+int ddfs(int u,int t,int flow){
+	if(u==t){
+		return flow;
 	}
+	while(ptr[u]<graph[u].size()){
+		edge &e=graph[u][ptr[u]];
+		if(dist[e.destination]==dist[u]+1 && e.flow<e.capacity){
+			int tflow=ddfs(e.destination,t,e.capacity-e.flow);
+			if(tflow>0){
+				e.flow+=tflow;
+				graph[e.destination][e.rev_pos].flow-=tflow;
+				return tflow;
+			}
+		}
+		ptr[u]++;
+	}
+	return 0;
 }
 
 int max_flow(int s,int t,int n){
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			res[i][j]=graph[i][j];
-		}
+	int max_flow=0;
+	while(dbfs(s,t,n)){
+		ptr.assign(n,0);
+		int tflow;
+		do{
+			tflow=ddfs(s,t,INF);
+			max_flow+=tflow;
+		}while(tflow!=0);
 	}
-	int mf=0;
-	while(true){
-		int f=0;
-		vector<int> dist(n,INF);
-		dist[s]=0;
-		queue<int> q;
-		q.push(s);
-		parent.assign(n,-1);
-		while(!q.empty()){
-			int u=q.front();
-			q.pop();
-			if(u==t) break;
-			for(int v=0;v<n;v++){
-				if(res[u][v]>0 && dist[v]==INF){
-					dist[v]=dist[u]+1;
-					q.push(v);
-					parent[v]=u;
-				}
-			}
-		}
-		augment(t,s,INF,f);
-		if(f==0) break;
-		mf+=f;
-	}
-	return mf;
+	return max_flow;
 }
 
 int main(){
-	
-	cleargraph(7,7,0);
-	graph[0][1]=100;
-	graph[0][5]=1;
-	graph[1][2]=20;
-	graph[1][4]=10;
-	graph[2][4]=20;
-	graph[2][3]=10;
-	graph[5][6]=10;
-	graph[6][4]=1;
-
+	add_edge(0,1,100);
+	add_edge(0,5,1);
+	add_edge(1,2,20);
+	add_edge(1,4,10);
+	add_edge(2,4,20);
+	add_edge(2,3,10);
+	add_edge(5,6,10);
+	add_edge(6,4,1);
 	cout<<max_flow(0,4,7)<<endl;
 }
