50fc25108819bbf7c501f5e82ba96808f7d02711
arunrajora <rajora.rajora2@outlook.com>
2017-03-02 17:53:39 +0530

    add stack code.

---
+++ codes/data structures/stack.cpp
@@ -0,0 +1,74 @@
+
+// Stack
+
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	node* next;
+	node* prev;
+};
+
+class stack{
+	public:
+		node* root;
+		int count;
+		
+		stack(){
+			root=NULL;
+			count=0;
+		}
+		
+		//true for empty
+		bool isempty(){
+			return root==NULL;
+		}
+		
+		int size(){
+			return count;
+		}
+		
+		void push(int val){
+			node *x=new node();
+			if(x==NULL){
+				throw "memory limit exceeded";
+			}
+			x->data=val;
+			x->prev=NULL;
+			x->next=root;
+			root=x;
+			count++;
+		}
+		
+		int pop(){
+			if(root==NULL){
+				throw "underflow";
+			}
+			int val=root->data;
+			node* x=root;
+			if(x->next==NULL){
+				root=NULL;
+			}
+			else{
+				x->next->prev=NULL;
+				root=x->next;
+			}
+			count--;
+			return val;
+		}
+		
+};
+int main(){
+	stack x;
+	x.push(1);
+	x.push(2);
+	x.push(3);
+	cout<<x.size()<<endl;
+	cout<<x.pop()<<endl;
+	cout<<x.pop()<<endl;
+	cout<<x.pop()<<endl;
+	x.push(100);
+	cout<<x.pop()<<endl;
+	
+}
