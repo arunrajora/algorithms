d9594ea6947aec4ce5ae2cc98a35e1525f799c37
arunrajora <rajora.rajora2@outlook.com>
2017-03-08 19:20:52 +0530

    update output file.

--- outputs/CODES_LIST.cpp
+++ outputs/CODES_LIST.cpp
@@ -855,6 +855,98 @@ int main(){
 
 // __________________________
 
+// .\codes\data structures\queue.cpp
+
+// Queue
+
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	node* next;
+	node* prev;
+};
+
+class queue{
+	public:
+		node* root;
+		node* tail;
+		int count;
+		
+		queue(){
+			root=NULL;
+			tail=NULL;
+			count=0;
+		}
+		
+		//true for empty
+		bool isempty(){
+			return root==NULL;
+		}
+		
+		int size(){
+			return count;
+		}
+		
+		void enqueue(int val){
+			node *x=new node();
+			if(x==NULL){
+				throw "memory limit exceeded";
+			}
+			x->data=val;
+			x->prev=tail;
+			x->next=NULL;
+			if(tail!=NULL){
+				tail->next=x;
+			}
+			if(root==NULL){
+				root=x;
+			}
+			tail=x;
+			count++;
+		}
+		
+		int dequeue(){
+			if(root==NULL){
+				throw "underflow";
+			}
+			int val=root->data;
+			node* x=root;
+			if(x->next==NULL){
+				root=NULL;
+				tail=NULL;
+			}
+			else{
+				x->next->prev=NULL;
+				root=x->next;
+			}
+			delete x;
+			count--;
+			return val;
+		}
+		
+};
+int main(){
+	queue x;
+	x.enqueue(1);
+	x.enqueue(2);
+	x.enqueue(3);
+	cout<<"size"<<x.size()<<endl;
+	cout<<x.dequeue()<<endl;
+	cout<<x.dequeue()<<endl;
+	cout<<x.dequeue()<<endl;
+	x.enqueue(100);
+	cout<<x.dequeue()<<endl;
+	
+}
+
+// __________________________
+
+
+
+// __________________________
+
 // .\codes\data structures\segment_tree.cpp
 // Segment tree with lazy propogation (max in range query)
 
@@ -1109,6 +1201,7 @@ class stack{
 				x->next->prev=NULL;
 				root=x->next;
 			}
+			delete x;
 			count--;
 			return val;
 		}
