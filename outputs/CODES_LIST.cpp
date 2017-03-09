e2be5d783e82a2ee928bd9925ec8a19b6df4d8a9
arunrajora <rajora.rajora2@outlook.com>
2017-03-09 19:36:14 +0530

    update output file.

--- outputs/CODES_LIST.cpp
+++ outputs/CODES_LIST.cpp
@@ -3532,6 +3532,42 @@ int main(){
 
 // __________________________
 
+// .\codes\maths\power_mod.cpp
+
+// Power of a number mod a number
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#define ull long long
+
+ull pow(ull a,ull b,ull mod){
+	if(b==0){
+		return 1;
+	}
+	else{
+		ull val=pow(a,b/2,mod);
+		val=(val*val+mod)%mod;
+		if(b%2){
+			val=(val*a+mod)%mod;
+		}
+		return val%mod;
+	}
+}
+
+int main(){
+	cout<<pow(2,4,3)<<endl;
+	cout<<pow(2,10,5)<<endl;
+	cout<<pow(3,3,27)<<endl;
+}
+
+// __________________________
+
+
+
+// __________________________
+
 // .\codes\maths\prime_factors.cpp
 
 // prime factors / prime divisors of a number
